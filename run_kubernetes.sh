1944b83597edc59278198e6aa87ef8e9fd5b8f11
Hariprakash-devops <87257154+Hariprakash-devops@users.noreply.github.com>
2021-08-14 11:09:28 +0530

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-dockerpath=kelanik8/capstone_project
+dockerpath=hariprakashvk/capstone_project
 TAG=capstoneproject
 
 kubectl run $TAG --image=$dockerpath --generator=run-pod/v1
